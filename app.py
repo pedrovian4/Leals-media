88049ae78b0f5735a6e0972c59e5f378c6963f00
Pedro Viana <pedrovianaasking@gmail.com>
2022-07-04 19:57:34 -0300

    Ianca não deica terminar isso aquiiiiiii

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import (Flask,
+                   render_template,
+                   redirect, 
+                   request, 
+                   session, 
+                   flash)
+from flask_sqlalchemy  import SQLAlchemy
+from flask_migrate  import Migrate
+from dotenv import load_dotenv
+from os import  getenv, urandom 
+
+
+
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config['SECRET_KEY']= urandom(24)
+    app.config['SQLALCHEMY_DATABASE_URI']=getenv('POSTGRES')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    db = SQLAlchemy(app=app)
+    Migrate(app= app, db = db)
+    
+    class User(db.Model): 
+        id = db.Column(db.Integer, primary_key= True)
+        name = db.Column(db.String, nullable = False)
+        email = db.Column(db.String, nullable = False)
+        password = db.Column(db.String, nullable= False)
+    
+    class Post (db.Model): 
+        id = db.Column(db.Integer, primary_key = True)
+        post = db.Column(db.String, nullable= False)
+        user_who_sent = db.Column(db.String, nullable = False)
+    
+    
+    
+    
+    @app.route('/')
+    def index():
+        return redirect('/signup')
+    
+  
+    @app.route('/signup',methods=['GET','POST'])
+    def signup():
+        if request.method == 'POST':
+            form = request.form   
+            u = User(name = form['name'], email = form['email'], password = form['password'])
+            
+            try: 
+                db.session.add(u)
+                db.session.commit()
+            
+            # Tratamento de um erro  geral  em execução
+            # Desvantagem: Menos Otimização de codigo -> Perda de perfomance
+            # Vantagem: Flexibilidade de erros
+            except Exception: 
+                    return '<h1>OPS! Ocorreu um erro</h3>'
+              
+            
+            
+        return render_template('signup.html')
+
+    @app.route('/login', methods =['GET','POST'])
+    def login():
+        #evento de envio de um verbo http
+        if request.method== 'POST': 
+            form = request.form 
+            try: 
+                u = User.query.filter_by(email = form['email'], password = form['password']).first()
+                if u: 
+                    session['logged']=True
+                    session['name']= u.name
+                    print(u)
+                    return redirect('/feed')
+                session.pop('_flash', None)
+                flash('Usuario não cadastrado')
+                return redirect('/login')
+            except Exception as e:
+                print(e) 
+                return redirect('/login')
+        return render_template('login.html')
+
+ 
+    @app.route('/feed',methods = ['GET','POST'])
+    def feed():
+        return  render_template('feed.html')
+
+    return app
